a17b62f77a6d5a8d8d9d96d52818418bcd832ac9
Mohanad Saleh hamed Alhattami <117485877+mohanadsaleh@users.noreply.github.com>
2023-01-04 13:57:18 +0300

    Add files via upload

---
+++ sekiz tabanina cervirme.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int sayi;
+    printf("Pozitif tamsayi girin: ");
+    scanf("%d", &sayi);
+
+    printf("Octal karsiligi: ");
+    printf("%o\n", sayi);
+
+    return 0;
+}
+
