3254517fa8df58cf6991ae8b7e9bdc87f0b68650
Mohanad Saleh hamed Alhattami <117485877+mohanadsaleh@users.noreply.github.com>
2023-01-04 12:56:06 +0300

    Add files via upload

---
+++ yildiz yazdirma.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+
+{
+
+char cumle[100];
+int uzunluk , i=0;
+uzunluk=str_Len(cumle);
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
